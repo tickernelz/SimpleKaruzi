64cad3d9beae17fc365d7e4463fe473402e8ae27
Zhafron Kautsar <zhafronadani@gmail.com>
2026-02-08 15:15:16 +0700

    chore(updater): bump version to 1.1.0

--- updater.py
+++ updater.py
@@ -13,7 +13,7 @@ from PyQt6.QtCore import QThread, pyqtSignal, QObject
 
 from Scripts.custom_dialogs import show_update_dialog, show_info, show_confirmation
 
-CURRENT_VERSION = "1.0.2"
+CURRENT_VERSION = "1.1.0"
 UPDATE_JSON_URL = "https://next.oclpapi.simplehac.cn/SKSP/update.json"
 
 def version_compare(remote_ver, local_ver):
